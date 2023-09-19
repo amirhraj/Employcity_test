3923afc36763c6a4e23771ba405a45d80a103b2b
Radzhab <radgab712@gmail.com>
2023-09-19 20:15:55 +0300

    change

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const selectTrigger = document.querySelector('.select-trigger');
+const selectOptions = document.querySelector('.select-options');
 
+
+selectTrigger.addEventListener('click', () => {
+  selectOptions.style.display = selectOptions.style.display === 'none' ? 'block' : 'none';
+});
+
+// Обработка выбора опции
+const selectOptionElements = document.querySelectorAll('.select-option');
+selectOptionElements.forEach((option) => {
+  option.addEventListener('click', () => {
+    selectTrigger.textContent = option.textContent;
+    selectOptions.style.display = 'none';
+  });
+});
+
+function handleSliderChange(event) {
+    let value = event.target.value;
+    document.getElementById("sliderValue").textContent =`${value}%`;
+
+  }
